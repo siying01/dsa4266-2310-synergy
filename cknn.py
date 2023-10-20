26ff960c39287687102681e3837078fe804e306f
Jia Yee Ong <ong.jiayee@u.nus.edu>
2023-10-20 13:44:13 +0800

    Implement aggregation of majority samples in training sets

--- cknn.py
+++ cknn.py
@@ -1,6 +1,3 @@
-### kiv
-# using smaller file
-
 '''Import libraries'''
 
 from pathlib import Path
@@ -26,9 +23,7 @@ dat = dat[['transcript_id', 'transcript_position', '6-seq',
            'time_1', 'stddev_1', 'mean_current_1',
            'time_2', 'stddev_2', 'mean_current_2',
            'time_3', 'stddev_3', 'mean_current_3',
-           'gene_id', 'label', 'folds_y']]
-
-dat.rename(columns={'folds_y': 'folds'}, inplace=True)
+           'gene_id', 'label', 'folds']]
 
 dat['transcript_id_position'] = dat['transcript_id'].astype(str) + '-' \
                              + dat['transcript_position'].astype(str)
@@ -48,28 +43,28 @@ def get_pr_auc(y_true, y_pred):
 
 '''Distance metric'''
 
-def get_hausdorff_distance(X_train, X_test, train_bags, test_bags, mode='max', quantile=0.7):
-  pairwise_distance = pairwise_distances(X_test, X_train) # returns matrix of dist btw a X_test instance, and X_train instance
+def get_hausdorff_distance(X_train, X_test, train_bags, test_bags, mode='max'):
   all_bag_distances = []
 
   for test_bag in tqdm(test_bags, total=len(test_bags)):
-    test_bag_instances = pairwise_distance[test_bag] # extract all the instances related to a particular test_bag, and get the distances from the test_bag instances to all train_bag instances
     bag_distances = []
 
     for train_bag in train_bags:
+      test_bag_instances = pairwise_distances(X_test[test_bag], X_train[train_bag]) # extract all the instances related to a particular test_bag, and get the distances from the test_bag instances to all train_bag instances
+
       if mode == 'max':
-        test_train_distance = np.max(np.min(test_bag_instances[:, train_bag], axis=1))
-        train_test_distance = np.max(np.min(test_bag_instances[:, train_bag].transpose(), axis=1))
+        test_train_distance = np.max(np.min(test_bag_instances, axis=1))
+        train_test_distance = np.max(np.min(test_bag_instances.transpose(), axis=1))
         bag_distances.append(max(test_train_distance, train_test_distance))
 
       elif mode =='avg':
-        test_train_distance = np.min(test_bag_instances[:, train_bag], axis=1)
-        train_test_distance = np.min(test_bag_instances[:, train_bag].transpose(), axis=1)
+        test_train_distance = np.min(test_bag_instances, axis=1)
+        train_test_distance = np.min(test_bag_instances.transpose(), axis=1)
         bag_distances.append(np.mean(np.concatenate([train_test_distance, test_train_distance])))
 
       elif mode == 'min':
-        test_train_distance = np.min(np.min(test_bag_instances[:, train_bag], axis=1))
-        train_test_distance = np.min(np.min(test_bag_instances[:, train_bag].transpose(), axis=1))
+        test_train_distance = np.min(np.min(test_bag_instances, axis=1))
+        train_test_distance = np.min(np.min(test_bag_instances.transpose(), axis=1))
         bag_distances.append(max(test_train_distance, train_test_distance))
 
     all_bag_distances.append(np.array(bag_distances).reshape(1, -1))
@@ -116,6 +111,8 @@ def get_citation_knn_pred_normalized(train_labels, test_labels, all_bag_distance
   citer_nn_labels = get_citation_counts(train_labels, test_labels, all_bag_distances, n_cit_neighbors)
   return reference_nn_labels / np.sum(reference_nn_labels, axis=0) + citer_nn_labels / np.sum(citer_nn_labels, axis=0)
 
+'''Preparation for training'''
+
 # Create dictionaries for numeric coding of distance and model
 # results are stored in numpy array before conversion to df
 # only numeric in the numpy array
@@ -128,7 +125,7 @@ r = 0
 
 # set probability threshold for class prediction
 knn_threshold_value = 0.5
-cknn_threshold_value = 0
+cknn_threshold_value = 0 # not set yet
 
 # Instantiate array to store results
 
@@ -146,9 +143,13 @@ cknn_threshold_value = 0
 
 arr_res = np.zeros((5*3*3, 13)) # num folds * num distances * num models
 
+# store imbalance per fold
+train_fold_labels = {}
+
 '''Cross validation'''
 
 for i in range(5):
+
   print(f"cross validation run: {i+1}")
 
   '''Prepare data'''
@@ -157,18 +158,42 @@ for i in range(5):
   dat_train = dat[dat['folds'] != i+1]
   dat_test = dat[dat['folds'] == i+1]
 
+  # downsample 'negative' 'instances'
+  dat_train_min = dat_train[dat_train['label'] == 1]
+  dat_train_maj = dat_train[dat_train['label'] == 0]
+  dat_train_maj_agg = dat_train_maj.groupby(['transcript_id_position']).agg({'time_1': 'mean', 
+                                                            'stddev_1': 'mean', 
+                                                            'mean_current_1': 'mean', 
+                                                            'time_2': 'mean', 
+                                                            'stddev_2': 'mean', 
+                                                            'mean_current_2': 'mean',
+                                                            'time_3': 'mean', 
+                                                            'stddev_3': 'mean', 
+                                                            'mean_current_3': 'mean',
+                                                            'label': 'first', 
+                                                            'folds': 'first'})
+  dat_train = pd.concat([dat_train_maj_agg, dat_train_min], axis = 0)
+
+  ### try to reduce RAM here
+  del dat_train_min
+  del dat_train_maj
+  del dat_train_maj_agg
+
+  # Check: class imbalance in training set
+  train_fold_labels[i+1] = dat_train.label.value_counts().to_dict()
+
   # dat_train-or-test: df of 9 features, labels, transcript id and position, 6-seq, gene id
   dat_train = dat_train.drop(['folds'], axis = 1)
   dat_test = dat_test.drop(['folds'], axis = 1)
 
   # reset index to make sure indexes pair with number of rows
-  dat_train = dat_train.reset_index(drop = True)  
-  dat_test = dat_test.reset_index(drop = True) 
+  dat_train = dat_train.reset_index(drop = True)
+  dat_test = dat_test.reset_index(drop = True)
 
   # df_train-or-test: df of 9 features and labels
-  df_train = dat_train.drop(['transcript_id', 'transcript_position', '6-seq', 
+  df_train = dat_train.drop(['transcript_id', 'transcript_position', '6-seq',
                              'gene_id', 'transcript_id_position'], axis = 1)
-  df_test = dat_test.drop(['transcript_id', 'transcript_position', '6-seq', 
+  df_test = dat_test.drop(['transcript_id', 'transcript_position', '6-seq',
                            'gene_id', 'transcript_id_position'], axis = 1)
 
   # arr_train-or-test: np array of 9 features
@@ -181,11 +206,9 @@ for i in range(5):
   X_train = scaler.transform(arr_train)
   X_test = scaler.transform(arr_test)
 
-  ## get training bag instances, bag labels 
+  ## get training bag instances, bag labels
   ## and bags corresponding to transcript-id-position
-
-  dict_train_bags = {}
-  dict_train_labels = {}
+  dict_train_bags, dict_train_labels = {}, {}
 
   for index, row in tqdm(dat_train.iterrows(), total = len(dat_train)):
     if row['transcript_id_position'] not in dict_train_bags:
@@ -198,12 +221,14 @@ for i in range(5):
   train_labels = np.array([v for v in dict_train_labels.values()])
   train_bags = np.array([v for v in dict_train_bags.values()])
   train_bags_number = np.array([k for k in dict_train_labels.keys()])
+  
+  ### try to reduce RAM here
+  del dict_train_bags
+  del dict_train_labels
 
-  ## get testing bag instances, bag labels 
+  ## get testing bag instances, bag labels
   ## and bags corresponding to transcript-id-position
-
-  dict_test_bags = {}
-  dict_test_labels = {}
+  dict_test_bags, dict_test_labels = {}, {}
 
   for index, row in tqdm(dat_test.iterrows(), total = len(dat_test)):
     if row['transcript_id_position'] not in dict_test_bags:
@@ -217,6 +242,10 @@ for i in range(5):
   test_bags = np.array([v for v in dict_test_bags.values()])
   test_bags_number = np.array([k for k in dict_test_labels.keys()])
 
+  ### try to reduce RAM here
+  del dict_test_bags
+  del dict_test_labels
+
   '''Compute bag distances'''
   bag_distances_max = get_hausdorff_distance(X_train, X_test, train_bags, test_bags, mode='max')
   #bag_distances_avg = get_hausdorff_distance(X_train, X_test, train_bags, test_bags, mode='avg')
@@ -228,9 +257,8 @@ for i in range(5):
 
   '''Run models'''
   # for each data set (CV), for each distance, perform 3 models
+  #for distance_mode, distance in zip(['max', 'avg', 'min'], [bag_distances_max, bag_distances_avg, bag_distances_min]):
   for distance_mode, distance in zip(['max'], [bag_distances_max]):
-  # for distance_mode, distance in zip(['max', 'avg', 'min'], [bag_distances_max, bag_distances_avg, bag_distances_min]):
-
     ### CHECK
     #print(f"fold is {i+1}, r is {r}, running distance is {distance_mode}")
 
@@ -243,7 +271,7 @@ for i in range(5):
     auc_pr = get_pr_auc(test_labels, y_pred_test)
     precision = precision_score(test_labels, y_pred_test, zero_division = 0.0)
     recall = recall_score(test_labels, y_pred_test, zero_division = 0.0)
-    f1 = f1_score(test_labels, y_pred_test, zero_division = 0.0) 
+    f1 = f1_score(test_labels, y_pred_test, zero_division = 0.0)
     tn, fp, fn, tp = confusion_matrix(test_labels, y_pred_test, labels = [0, 1]).ravel()
     arr_res[r, :] = arr_temp + [dict_model['kNN'], balanced_acc, auc_roc, auc_pr, precision, recall, f1, tn, fp, fn, tp]
     r += 1
@@ -256,12 +284,12 @@ for i in range(5):
     auc_pr = get_pr_auc(test_labels, y_pred_test)
     precision = precision_score(test_labels, y_pred_test, zero_division = 0.0)
     recall = recall_score(test_labels, y_pred_test, zero_division = 0.0)
-    f1 = f1_score(test_labels, y_pred_test, zero_division = 0.0) 
+    f1 = f1_score(test_labels, y_pred_test, zero_division = 0.0)
     tn, fp, fn, tp = confusion_matrix(test_labels, y_pred_test, labels = [0, 1]).ravel()
     arr_res[r, :] = arr_temp + [dict_model['ckNN'], balanced_acc, auc_roc, auc_pr, precision, recall, f1, tn, fp, fn, tp]
     r += 1
 
-    # citation kNN with normalised 
+    # citation kNN with normalised
     y_counts_test = get_citation_knn_pred_normalized(train_labels, test_labels, distance, n_neighbors_ref)
     y_pred_test = np.argmax(y_counts_test, axis = 1)
     balanced_acc = balanced_accuracy_score(test_labels, y_pred_test)
@@ -269,10 +297,12 @@ for i in range(5):
     auc_pr = get_pr_auc(test_labels, y_pred_test)
     precision = precision_score(test_labels, y_pred_test, zero_division = 0.0)
     recall = recall_score(test_labels, y_pred_test, zero_division = 0.0)
-    f1 = f1_score(test_labels, y_pred_test, zero_division = 0.0) 
+    f1 = f1_score(test_labels, y_pred_test, zero_division = 0.0)
     tn, fp, fn, tp = confusion_matrix(test_labels, y_pred_test, labels = [0, 1]).ravel()
     arr_res[r, :] = arr_temp + [dict_model['norm-ckNN'], balanced_acc, auc_roc, auc_pr, precision, recall, f1, tn, fp, fn, tp]
     r += 1
+
+'''Get results'''
 
 df_res = pd.DataFrame(arr_res, columns=['Validation on Fold','Distance Type', 'Model',
                                           'AUC ROC', 'AUC PRC', 'Precision', 'Recall', 'F1', 'Balanced Accuracy',
@@ -290,3 +320,6 @@ df_res.loc[df_res['Model'] == 6, 'Model'] = 'norm-ckNN'
 df_res.sort_values(by=['Model', 'Distance Type', 'Validation on Fold']).to_csv("res_full.csv")
 
 df_res.groupby(['Model', 'Distance Type']).apply(lambda x: np.mean(x.iloc[:, 3:], axis = 0)).to_csv("res_averaged.csv")
+
+with open('train_fold_labels', 'w') as f:
+    print(train_fold_labels, file=f)
